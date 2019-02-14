ce443ac4dc0abf942746cc11c313127748cca120
Taiki Endo <te316e89@gmail.com>
2019-02-15 00:52:34 +0900

    Simplify examples

--- examples/const_fn/lib.rs
+++ examples/const_fn/lib.rs
@@ -1,5 +1,3 @@
-// These codes copied from https://github.com/taiki-e/const_fn/blob/master/src/lib.rs
-
 extern crate proc_macro;
 extern crate proc_macro2;
 #[macro_use]
@@ -9,22 +7,17 @@ extern crate syn_mid;
 
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use quote::ToTokens;
 use syn::{parse_macro_input, parse_quote};
 use syn_mid::ItemFn;
 
-fn compile_err(msg: &str) -> TokenStream {
-    quote!(compile_error!(#msg);).into()
-}
+// These codes copied from https://github.com/taiki-e/const_fn/blob/master/src/lib.rs
 
+/// An attribute for easy generation of a const function with conditional compilations.
 #[proc_macro_attribute]
 pub fn const_fn(args: TokenStream, function: TokenStream) -> TokenStream {
-    if args.is_empty() {
-        return compile_err("requires an argument");
-    }
+    assert!(!args.is_empty(), "requires an argument");
 
     let mut function: ItemFn = parse_macro_input!(function);
-
     let mut const_function = function.clone();
 
     if function.constness.is_some() {
@@ -37,7 +30,5 @@ pub fn const_fn(args: TokenStream, function: TokenStream) -> TokenStream {
     function.attrs.push(parse_quote!(#[cfg(not(#args))]));
     const_function.attrs.push(parse_quote!(#[cfg(#args)]));
 
-    let mut function = function.into_token_stream();
-    function.extend(const_function.into_token_stream());
-    function.into()
+    quote!(#function, #const_function).into()
 }
