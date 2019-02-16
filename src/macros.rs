b56d1b87c66b523cf2a8692501e771028f5cbef6
Taiki Endo <te316e89@gmail.com>
2019-02-16 21:10:49 +0900

    Cleanup macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,34 +1,12 @@
 macro_rules! ast_struct {
     (
         $(#[$attr:meta])*
-        pub struct $name:ident #manual_extra_traits $($rest:tt)*
-    ) => {
-        $(#[$attr])*
-        #[cfg_attr(feature = "clone-impls", derive(Clone))]
-        pub struct $name $($rest)*
-    };
-
-    (
-        $(#[$attr:meta])*
         pub struct $name:ident $($rest:tt)*
     ) => {
         $(#[$attr])*
         #[cfg_attr(feature = "clone-impls", derive(Clone))]
         pub struct $name $($rest)*
     };
-}
-
-macro_rules! ast_enum {
-    (
-        $(#[$enum_attr:meta])*
-        pub enum $name:ident $(# $tags:ident)* { $($variants:tt)* }
-    ) => (
-        $(#[$enum_attr])*
-        #[cfg_attr(feature = "clone-impls", derive(Clone))]
-        pub enum $name {
-            $($variants)*
-        }
-    )
 }
 
 macro_rules! ast_enum_of_structs {
@@ -43,14 +21,13 @@ macro_rules! ast_enum_of_structs {
 
         $($remaining:tt)*
     ) => (
-        ast_enum! {
-            $(#[$enum_attr])*
-            pub enum $name {
-                $(
-                    $(#[$variant_attr])*
-                    $variant $( ($member) )*,
-                )*
-            }
+        $(#[$enum_attr])*
+        #[cfg_attr(feature = "clone-impls", derive(Clone))]
+        pub enum $name {
+            $(
+                $(#[$variant_attr])*
+                $variant $( ($member) )*,
+            )*
         }
 
         $(
@@ -80,8 +57,6 @@ macro_rules! ast_enum_of_structs {
 }
 
 macro_rules! generate_to_tokens {
-    (do_not_generate_to_tokens $($foo:tt)*) => ();
-
     (($($arms:tt)*) $tokens:ident $name:ident { $variant:ident, $($next:tt)*}) => {
         generate_to_tokens!(
             ($($arms)* $name::$variant => {})
@@ -91,7 +66,7 @@ macro_rules! generate_to_tokens {
 
     (($($arms:tt)*) $tokens:ident $name:ident { $variant:ident [$($rest:tt)*], $($next:tt)*}) => {
         generate_to_tokens!(
-            ($($arms)* $name::$variant(ref _e) => to_tokens_call!(_e, $tokens, $($rest)*),)
+            ($($arms)* $name::$variant(ref _e) => ::quote::ToTokens::to_tokens(_e, $tokens),)
             $tokens $name { $($next)* }
         );
     };
@@ -104,12 +79,6 @@ macro_rules! generate_to_tokens {
                 }
             }
         }
-    };
-}
-
-macro_rules! to_tokens_call {
-    ($e:ident, $tokens:ident, $($rest:tt)*) => {
-        ::quote::ToTokens::to_tokens($e, $tokens)
     };
 }
 
